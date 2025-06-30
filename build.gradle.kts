22bbe62ead65e3739244dfa89875e3a41444e645
Jimin <jimin@users.noreply.github.com>
2025-06-30 18:09:17 +0000

    Fixed: increased chat char limit now works again

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.pwetutils"
-version = "1.1.3"
+version = "1.1.4"
 
 minecraft.version("1.8.9")
 
--- src/main/java/com/pwetutils/mixins/ChatPacketMixin.java
+++ src/main/java/com/pwetutils/mixins/ChatPacketMixin.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(C01PacketChatMessage.class)
@@ -15,9 +16,16 @@ public class ChatPacketMixin {
     @Mutable
     private String message;
 
-    // increase char limit & handle chat emotes
+    @Redirect(
+            method = "<init>(Ljava/lang/String;)V",
+            at = @At(value = "INVOKE", target = "Ljava/lang/String;substring(II)Ljava/lang/String;")
+    )
+    private String increaseChatLimit(String message, int start, int end) {
+        return message.substring(0, Math.min(256, message.length()));
+    }
+
     @Inject(method = "<init>(Ljava/lang/String;)V", at = @At("RETURN"))
     private void processEmotes(String message, CallbackInfo ci) {
-        this.message = EmoteHandler.processEmotes(this.message.substring(0, Math.min(256, this.message.length())));
+        this.message = EmoteHandler.processEmotes(this.message);
     }
 }
